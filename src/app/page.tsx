024fa887511f829d7b23b01a00f3fe07659f82a4
sunny <sunny@bytespark.app>
2024-12-05 14:06:15 +0800

    feat: documents page and tiptap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen justify-center items-center ">
       <Button variant="destructive">Hello World</Button>
+      <Link href="/documents/123">Click To Documents Id</Link>
     </div>
   );
 }
