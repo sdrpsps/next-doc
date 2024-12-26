12438b71dda257df013e1ab82027b6834cc3689b
sunny <sunny@bytespark.app>
2024-12-26 15:07:15 +0800

    feat: convex database setup

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+
+import { useQuery } from "convex/react";
 import { Navbar } from "./navbar";
 import { TemplatesGallery } from "./templates-gallery";
+import { api } from "../../../convex/_generated/api";
 
 export default function Home() {
+  const documents = useQuery(api.document.get);
+
+  if (!documents) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -9,6 +19,7 @@ export default function Home() {
       </div>
       <div className="mt-16">
         <TemplatesGallery />
+        {documents?.map((doc) => <span key={doc._id}>{doc.title}</span>)}
       </div>
     </div>
   );
