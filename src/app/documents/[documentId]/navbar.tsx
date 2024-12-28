a54ced103a3f7be0b83cc948c87dd0d723c1c4ab
sunny <sunny@bytespark.app>
2024-12-28 16:08:56 +0800

    feat: populate user information

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -37,6 +37,7 @@ import { BsFilePdf } from "react-icons/bs";
 import { DocumentInput } from "./document-input";
 import { useEditorStore } from "@/store/use-editor-store";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { Avatars } from "./avatars";
 
 export const Navbar = () => {
   const { editor } = useEditorStore();
@@ -268,6 +269,7 @@ export const Navbar = () => {
         </div>
       </div>
       <div className="flex items-center gap-3 pl-6">
+        <Avatars />
         <OrganizationSwitcher
           afterCreateOrganizationUrl="/new"
           afterLeaveOrganizationUrl="/"
